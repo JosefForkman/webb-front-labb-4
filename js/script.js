bffffa78d8c4f56e452680cc3257722bed8d8def
JosefForkman <44498331+JosefForkman@users.noreply.github.com>
2025-03-20 14:59:43 +0100

    add js to links bord

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ const dashbordWrapper = document.querySelector(".dashbord-wrapper");
 const dashbordTitle = dashbordWrapper.querySelector(".dashboard-name");
 const dashbordForm = dashbordWrapper.querySelector("form");
 
+const linksContiner = document.querySelector(".links-bord ul");
+const addLinkButton = document.querySelector(".add-link");
+const addLinkForm = document.querySelector(".add-link-form");
+
 /* Local Storage */
 const dashboardData = JSON.parse(localStorage.getItem("dashboardData")) || {
     title: "John Doe Dashboard",
@@ -77,4 +81,59 @@ dashbordTitle.addEventListener("click", () => {
     dashbordForm.querySelector("input").value = dashboardData.title;
     dashbordTitle.style.display = "none";
     dashbordForm.style.display = "flex";
+});
+
+/* Links */
+
+const createLinkElement = (link) => {
+    const linkElement = document.createElement("li");
+
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = `<span class="sr-only">Ta bort</span>
+        <i class="fa-solid fa-circle-minus"></i>`;
+    removeButton.addEventListener("click", () => {
+        dashboardData.Links = dashboardData.Links.filter(
+            (filterLink) => filterLink !== link,
+        );
+        updateDashbordLocalStorge();
+        linkElement.remove();
+    });
+    linkElement.innerHTML = `
+    <img
+    src="${link.favicon}"
+    alt="Google logo" />
+    <p><a href="${link.url}" target="_blank">${link.name}</a></p>
+    `;
+    linkElement.appendChild(removeButton);
+    linksContiner.appendChild(linkElement);
+};
+
+linksContiner.innerHTML = "";
+dashboardData.Links.forEach((link) => {
+    createLinkElement(link);
+});
+
+addLinkButton.addEventListener("click", () => {
+    addLinkButton.nextElementSibling.showModal();
+});
+
+addLinkForm.addEventListener("submit", (event) => {
+    const formData = new FormData(addLinkForm);
+    const url = formData.get("link").trim();
+    const title = formData.get("title").trim();
+
+    if (url === "" || title === "") {
+        return;
+    }
+
+    const link = {
+        name: title,
+        url,
+        favicon: `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${url}&size=128`,
+    };
+    dashboardData.Links.push(link);
+    updateDashbordLocalStorge();
+    createLinkElement(link);
+
+    addLinkForm.reset();
 });
